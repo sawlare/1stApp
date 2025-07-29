bbb45ddc2a0d8bebb896ae899e959347e0471930
android <samiranurmash2010@gmail.com>
2025-07-29 19:58:35 +0300

    second commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -24,6 +24,10 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
+            activityResultRegistry.run {
+
+            }
         }
     }
 }
